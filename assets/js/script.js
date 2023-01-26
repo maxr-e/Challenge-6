d3c984b646d77c11ab6af70b69ac9b12da1db33b
unknown <maxroyal.eisenberg@gmail.com>
2023-01-25 22:11:36 -0800

    updated syntax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,73 @@
  // Global variables
- var movieList = [];
- var movieName;
+ var cityList = [];
+ var cityName;
  
 
 // local storage functions
-initMovieList();
+initcityList();
 initWikipedia();
 
-function renderMovies(){
+function rendercitys(){
     $("#list-of-cities").empty();
-    $("#movie-name").val("");
+    $("#city-name").val("");
     
-    for (i=0; i<movieList.length; i++){
+    for (i=0; i<cityList.length; i++){
         var a = $("<a>");
-        a.addClass("button is-inline px-4 m-4 movie");
-        a.attr("data-name", movieList[i]);
-        a.text(movieList[i]);
+        a.addClass("button is-inline px-4 m-4 city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
         $("#list-of-cities").prepend(a);
     } 
 }
-// This function pulls the movie list array from local storage
-function initMovieList() {
-    var storedMovies = JSON.parse(localStorage.getItem("movies"));
+// This function pulls the city list array from local storage
+function initcityList() {
+    var storedcitys = JSON.parse(localStorage.getItem("cities"));
     
-    if (storedMovies !== null) {
-        movieList = storedMovies;
+    if (storedcitys !== null) {
+        cityList = storedcitys;
     }
     
-    renderMovies();
+    rendercitys();
     }
-// This function pull the current movie into local storage to display the current info on reload
+// This function pull the current city into local storage to display the current info on reload
 function initWikipedia() {
     var storedWikipedia = JSON.parse(localStorage.getItem("wikipedia"));
 
     if (storedWikipedia !== null) {
-        movieName = storedWikipedia;
+        cityName = storedWikipedia;
 
         displayWikipedia();
     }
 }
 
-// This function saves the movie array to local storage
-function storeMovieArray() {
-    localStorage.setItem("movies", JSON.stringify(movieList));
+// This function saves the city array to local storage
+function storecityArray() {
+    localStorage.setItem("cities", JSON.stringify(cityList));
     }
 
-// This function saves the currently displayed movie to local storage
-function storeCurrentMovie() {
+// This function saves the currently displayed city to local storage
+function storeCurrentcity() {
 
-    localStorage.setItem("currentMovie", JSON.stringify(movieName));
+    localStorage.setItem("currentcity", JSON.stringify(cityName));
 }
       
-// Click event handler for movie search button
+// Click event handler for city search button
 $(".search-btn").on("click", function(event){
     event.preventDefault();
 
-    movieName = $("#movie-name").val().trim();
-    if(movieName === ""){
-        alert("Please enter a movie to look up")
+    cityName = $("#city-name").val().trim();
+    if(cityName === ""){
+        alert("Please enter a city to look up")
 
-    }else if (movieList.length >= 3){  
-        movieList.shift();
-        movieList.push(movieName);
+    }else if (cityList.length >= 3){  
+        cityList.shift();
+        cityList.push(cityName);
 
     }else{
-    movieList.push(movieName);
+    cityList.push(cityName);
     }
-    storeCurrentMovie();
-    storeMovieArray();
-    renderMovies();
+    storeCurrentcity();
+    storecityArray();
+    rendercitys();
     // displayWikipedia();
 });
